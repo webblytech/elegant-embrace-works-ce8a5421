5feaf9f9c8bcd9a2239e1f4406e5264e6d7b936a
Habsy33 <holuyemo@gmail.com>
2026-06-04 18:32:12 +0100

    removed nitro just to see

--- vite.config.ts
+++ vite.config.ts
@@ -7,10 +7,6 @@
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
-  nitro: {
-    preset: "vercel",
-  },
-
   tanstackStart: {
     server: {
       entry: "server",
